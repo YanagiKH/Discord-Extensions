657669b779c75fd0734eb301e91a8ab26c32490c
KH. <142028490+YanagiKH@users.noreply.github.com>
2026-07-31 14:44:29 +0900

    Add Electron Vite build config

---
+++ electron.vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'electron-vite';
+
+export default defineConfig({
+  main: {
+    build: {
+      outDir: 'dist/main'
+    }
+  },
+  preload: {
+    build: {
+      outDir: 'dist/preload'
+    }
+  },
+  renderer: {
+    build: {
+      outDir: 'dist/renderer'
+    }
+  }
+});
