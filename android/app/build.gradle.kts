c510276d005a773c1674362cf5e00a2f597096ae
KH. <142028490+YanagiKH@users.noreply.github.com>
2026-08-05 18:12:42 +0900

    Add JVM JSON dependency for Android unit tests

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation("androidx.recyclerview:recyclerview:1.3.2")
 
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.json:json:20240303")
     androidTestImplementation("androidx.test:core-ktx:1.6.1")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
