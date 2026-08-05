4719bf39d83b54b0aa309106009329f0c1a03d87
KH. <142028490+YanagiKH@users.noreply.github.com>
2026-08-05 17:56:31 +0900

    Add Java module template source

---
+++ mods/templates/java/src/main/java/module/ExtensionModule.java
@@ -0,0 +1,7 @@
+package module;
+
+public final class ExtensionModule {
+    public String execute(String input) {
+        return "{\"plugin\":\"java-module-template\",\"status\":\"ok\"}";
+    }
+}
